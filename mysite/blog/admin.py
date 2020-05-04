e83361499e8fbe0fa858bea264c906962a8054b4
Emilio Araos <ea3dev@gmail.com>
2020-05-04 02:06:07 -0600

    creating a form

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')      # Adds to the filter in the right
     search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title', )}     # Prepopulates slug with th title.
+    prepopulated_fields = {'slug': ('title', )}     # Pre-populates slug with th title.
     raw_id_fields = ('author', )        # inserts a lookup widget in the author field.
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')        # Orders automatically these two columns.
